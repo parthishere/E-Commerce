c0f9c8c15dbc1b9cff1cf3e4bfef5db4a37b4aa8
parthishere <parthishere1234@gmail.com>
2021-05-21 01:21:42 +0530

    feat: add billing model

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BillingProfile(models.Model):
+    user    = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    email = models.EmailField()
+    active = models.BooleanField(null=True, blank=True, default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
+    
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        BillingProfile.objects.get_or_create(user=instance)
+        
+post_save.connect(user_created_receiver, sender=User)
